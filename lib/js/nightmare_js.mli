ae594819c2b43c9c1e5454f37f2b0b1fc00db39e
xvw <xavier.vdw@gmail.com>
2023-05-28 11:21:27 +0200

    Move Interfaces and Bindings outside of the API
    Although it's likely that we'll discover a new form of convention later,
    at the moment we seem to be used to having a `Bindings` module that
    describes the low-level plumbing of a JS library.
    
    Re-exporting it from the root of the module, especially in Nightmare_js,
    creates conflicts with potential `Bindings` modules present in a library
    dependent on `Nightmare_js`.

--- lib/js/nightmare_js.mli
+++ lib/js/nightmare_js.mli
@@ -36,10 +36,10 @@ module Aliases = Aliases
 
 include module type of Aliases (** @inline *)
 
-(** {2 Modules types} *)
+(* (\** {2 Modules types} *\) *)
 
-module Bindings = Bindings
-module Interfaces = Interfaces
+(* module Bindings = Bindings *)
+(* module Interfaces = Interfaces *)
 
 (** {2 Optional values} *)
 
